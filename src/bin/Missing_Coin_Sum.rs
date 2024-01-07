d8e42ae8cd0f6d559ffb0bd449095704d832f652
Koushik Sahu <koushiksahu@microsoft.com>
2024-01-07 11:39:21 +0530

    add more solutions

---
+++ src/bin/Missing_Coin_Sum.rs
@@ -0,0 +1,78 @@
+use std::io::{prelude::BufRead, prelude::Write, stdin, stdout, BufWriter};
+use std::str;
+
+/*
+    Author: Koushik Sahu
+    Created: 20:52:27 IST 06-01-2024 Saturday
+*/
+
+fn solve<R: BufRead, W: Write>(sc: &mut Scanner<R>, wr: &mut W) {
+    let n: i32 = sc.next();
+    let mut x: Vec<i32> = (0..n).map(|_| sc.next()).collect();
+    x.sort();
+    let mut ans: i64 = 1;
+    for i in 0..x.len() {
+        if x[i] as i64 > ans {
+            break;
+        } else {
+            ans += x[i] as i64;
+        }
+    }
+    writeln!(wr, "{}", ans).unwrap();
+}
+
+#[macro_export]
+macro_rules! dbg{
+    ($(a:expr),*) => {
+        #[cfg(debug_assertions)]
+        eprintln!(
+            concat!("{}:{}:{}: ",$(stringify!(a), " = {:?}, "),*),
+            file!(), line!(), column!(), $(a),*
+            );
+        #[cfg(not(debug_assertions))]
+        {};
+    }
+}
+
+struct Scanner<R> {
+    reader: R,
+    buf_str: Vec<u8>,
+    buf_iter: str::SplitWhitespace<'static>,
+}
+
+impl<R: BufRead> Scanner<R> {
+    fn new(reader: R) -> Self {
+        Self {
+            reader,
+            buf_str: vec![],
+            buf_iter: "".split_whitespace(),
+        }
+    }
+    fn next<T: str::FromStr>(&mut self) -> T {
+        loop {
+            if let Some(token) = self.buf_iter.next() {
+                return token.parse().ok().expect("Failed parse");
+            }
+            self.buf_str.clear();
+            self.reader
+                .read_until(b'\n', &mut self.buf_str)
+                .expect("Failed read");
+            self.buf_iter = unsafe {
+                let slice = str::from_utf8_unchecked(&self.buf_str);
+                std::mem::transmute(slice.split_whitespace())
+            }
+        }
+    }
+}
+
+fn main() {
+    let (stdin, stdout) = (stdin(), stdout());
+    let mut scan = Scanner::new(stdin.lock());
+    let mut out = BufWriter::new(stdout.lock());
+    #[allow(unused_variables)]
+    let t: i32 = 1;
+    //let t: i32 = scan.next();
+    for _ in 0..t {
+        solve(&mut scan, &mut out);
+    }
+}
